9d31eb6ec0d4e6b273fd8ef8ec341a1649bb4dbf
Shaun47 <shaunhossain655@gmail.com>
2022-10-05 21:25:08 +0600

    Creating list and Detail views

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 def home(request):
-    return render(request,'home.html')
+    posts = Post.published.all()
+    context = {
+        'posts': posts
+    }
+    return render(request,'home.html',context)
