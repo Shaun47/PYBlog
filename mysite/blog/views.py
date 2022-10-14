aabe908b1b2522c4dc65bf825f15546eecb2904a
Shaun47 <shaunhossain655@gmail.com>
2022-10-14 18:14:21 +0600

    project-update

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render,get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from .models import Post
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from .forms import CommentForm
+from django.urls import reverse
+from django.shortcuts import redirect
+
+
+
+
 
 # Create your views here.
 def home(request):
@@ -34,24 +41,32 @@ def post_detail(request,year,month,day,post):
     
     comments = post.comments.filter(active=True)
     new_comment = None
+    comment_form = CommentForm()
+    context = {
+        'post':post,
+        'comments': comments,
+        'comment_form': comment_form 
+    }
     if request.method == 'POST':
-        # comment_form = CommentForm(data=request.POST)
-        # if comment_form.is_valid():
-        #     new_comment = comment_form.save(commit=False)
-        #     new_comment.post = post
-        #     new_comment.save()
-        
-        # else:
-        #     comment_form = CommentForm()
-        print(request.method.POST['name'])
-        exit()
-
+    #     if request.POST.is_valid():
+                
+        comment_form = CommentForm(data = request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+            context = {'post':post,
+                        'comments': comments, 
+                        'new_comment': new_comment,
+                        'comment_form': comment_form                                              
+                        
+                        
+                        }
+            return redirect(post) # this post is a canonical url. so we are passing just this object 
+                                # to redirect to a post
+    else:
+        comment_form = CommentForm()
+            
     
     
-    
-    return render(request,'single_page.html',{'post':post,
-                                                # 'comments': comments,
-                                                # 'new_comment': new_comment,
-                                                # 'comment_form': comment_form,
-                                              
-                                              })
+    return render(request,'single_page.html',context)
